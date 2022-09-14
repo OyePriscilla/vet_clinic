f6e370ad55d1051e3c678486edb01f40e9990ed9
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-09-14 10:52:35 +0100

    Update animals table with addition colums referencing species and owners ID resp.

--- schema.sql
+++ schema.sql
@@ -12,3 +12,20 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD species varchar(100);
+
+CREATE TABLE owners (
+    owners_id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(150) NOT NULL,
+    age INT,
+      PRIMARY KEY(owners_id)
+);
+
+CREATE TABLE species (
+    species_id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(150) NOT NULL,
+    PRIMARY KEY(species_id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(species_id);
+ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(owners_id);
