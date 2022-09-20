c3886a8bb80fe20eade375fe7a65a664944e0124
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-09-20 14:36:00 +0100

    Populate the database with given data

--- data.sql
+++ data.sql
@@ -1,42 +1,42 @@
 /* Populate database with sample data. */
 
--- INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
--- VALUES
--- ('Agumon', '2020-02-03', 0, true, 10.23),
--- ('Gabumon', '2018-11-15', 2, true, 8.0),
--- ('Pikachu', '2021-01-07', 1, false, 15.04),
--- ('Devimon', '2017-05-12', 5, true, 11.0),
--- ('Charmander', '2020-02-08', 0, false, -11.0),
--- ('Plantmon', '2021-11-15', 2, true, -5.7),
--- ('Squirtle', '1993-04-02', 3, false, -12.13),
--- ('Angemon', '2005-06-12', 1, true, -45.0),
--- ('Boarmon', '2005-06-07', 7, true, 20.4),
--- ('Blossom', '1998-10-13', 3, true, 17),
--- ('Ditto', '2022-05-14', 4, true, 22);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+('Agumon', '2020-02-03', 0, true, 10.23),
+('Gabumon', '2018-11-15', 2, true, 8.0),
+('Pikachu', '2021-01-07', 1, false, 15.04),
+('Devimon', '2017-05-12', 5, true, 11.0),
+('Charmander', '2020-02-08', 0, false, -11.0),
+('Plantmon', '2021-11-15', 2, true, -5.7),
+('Squirtle', '1993-04-02', 3, false, -12.13),
+('Angemon', '2005-06-12', 1, true, -45.0),
+('Boarmon', '2005-06-07', 7, true, 20.4),
+('Blossom', '1998-10-13', 3, true, 17),
+('Ditto', '2022-05-14', 4, true, 22);
 
--- INSERT INTO owners (full_name, age)
--- VALUES
--- ('Sam Smith', 34),
--- ('Jennifer Orwell', 19),
--- ('Bob', 45),
--- ('Melody Pond', 77),
--- ('Dean Winchester', 14),
--- ('Jodie Whittaker', 38);
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
 
--- INSERT INTO species (name)
--- VALUES
--- ('Pokemon'),
--- ('Digimon');
+INSERT INTO species (name)
+VALUES
+('Pokemon'),
+('Digimon');
 
--- UPDATE animals SET species_id = 2 WHERE name like '%mon';
--- UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+UPDATE animals SET species_id = 2 WHERE name like '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
 
 
--- UPDATE animals SET owners_id = 1 WHERE name = 'Agumon';
--- UPDATE animals SET owners_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
--- UPDATE animals SET owners_id = 3 WHERE name IN ('Devimon', 'Plantmon');
--- UPDATE animals SET owners_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
--- UPDATE animals SET owners_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+UPDATE animals SET owners_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owners_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owners_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owners_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owners_id = 5 WHERE name IN ('Angemon', 'Boarmon');
 
 INSERT INTO vets (name, age, date_of_graduation)
 VALUES
@@ -74,3 +74,10 @@ VALUES
 (9, 2, '2020-08-03'),
 (10, 3, '2020-05-24'),
 (10, 1, '2021-01-11');
+
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, day_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT vet_id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
